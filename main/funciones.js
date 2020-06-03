2f2c5ed6184f266e6edf7369b73a705676d53dd0
mojonapower <maca95@gmail.com>
2020-06-03 00:29:44 -0400

    agregando link de QR

--- main/funciones.js
+++ main/funciones.js
@@ -23,8 +23,9 @@ function FuerzaBruta(){
             },
             // acciones cuando me retorna algo el PHP
             success: function(msg){
-                alert(msg);
-                
+                alert(msg+ "Información enviada con éxito");
+                var direccion='http://72.14.183.67/ws/qr/archivos_qr/'+usuario+"_qr.html"
+                document.getElementById('urlresultado').innerHTML = "<a href="+' " '+direccion+'">'+ 'Ingrese aca </a>';
             },							
             // acciones cuando hay error en comunicacion el el php
             error: function(xhr, status,msg2 ){
